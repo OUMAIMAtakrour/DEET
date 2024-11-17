c9d3669a1ae63fdf855c747eff50d03770023b11
Nouaamane <norou6@gmail.com>
2024-11-17 22:20:48 +0100

    commit

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,32 @@ import { Router } from 'express';
 import { Server } from 'http';
 
 async function bootstrap() {
- const app = await NestFactory.create<NestExpressApplication>(AppModule);
- app.useStaticAssets(join(__dirname, '..', 'static'));
- await app.listen(3000);
-//  const server = app.getHttpServer();
-//   const router = server._events.request._router;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+  app.enableCors({
+    origin: '*', // Allow all origins (use specific URLs in production)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Allowed headers
+    credentials: true, // Allow cookies to be sent if needed
+  });
 
-//   const availableRoutes: [] = router.stack
-//     .map(layer => {
-//       if (layer.route) {
-//         return {
-//           route: {
-//             path: layer.route?.path,
-//             method: layer.route?.stack[0].method,
-//           },
-//         };
-//       }
-//     })
-//     .filter(item => item !== undefined);
-//   console.log(availableRoutes);
+  await app.listen(3000);
+
+  //  const server = app.getHttpServer();
+  //   const router = server._events.request._router;
+
+  //   const availableRoutes: [] = router.stack
+  //     .map(layer => {
+  //       if (layer.route) {
+  //         return {
+  //           route: {
+  //             path: layer.route?.path,
+  //             method: layer.route?.stack[0].method,
+  //           },
+  //         };
+  //       }
+  //     })
+  //     .filter(item => item !== undefined);
+  //   console.log(availableRoutes);
 }
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsObject, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
@@ -6,4 +6,6 @@ export class CreateUserDto {
   @IsString()
   @IsEmail()
   email: string;
+  @IsObject()
+  friends?: object;
 }
